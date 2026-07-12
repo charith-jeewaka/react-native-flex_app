81b961822b2737f7a190bea13194c05e1643e98e
W.A.Charith Jeewaka <wickramasinghajeewaka@gmail.com>
2026-07-12 15:28:03 +0530

    task model created

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your ap!</Text>
+      <Text>Open up App.tsto start working on your ap!</Text>
       <StatusBar style="auto" />
     </View>
   );
